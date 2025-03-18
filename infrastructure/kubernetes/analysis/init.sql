b345e8262098de5de78e3de204cf200178c80ce6
Jeonghyun109 <kjhrlawjdgus@kaist.ac.kr>
2025-03-18 22:49:35 +0900

    Add infrastructure code

---
+++ infrastructure/kubernetes/analysis/init.sql
@@ -0,0 +1,15 @@
+create table public.start_record
+(
+    id            bigserial    primary key,
+    connection_id varchar(255) not null,
+    timestamp     bigint       not null,
+    response_time bigint       not null
+);
+
+create table public.end_record
+(
+    id             bigserial    primary key,
+    connection_id  varchar(255) not null,
+    timestamp      bigint       not null,
+    inference_time bigint      not null
+);
